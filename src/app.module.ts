e93a38c525bed0cad0510afb51279c2421a5aff6
Raul <raulantsilveira@gmail.com>
2025-03-12 13:50:47 -0300

    feat: prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   imports: [],
   controllers: [AppController],
   // são todos os controllers deste módulo
-  providers: [AppService],
+  providers: [AppService, PrismaService],
   // todas as dependencia(inversao de dependencia/injectables) que os controllers deste modulo
   // podem vir a ter
 })
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+// extende a classe do PrismaClient pra termos acesso ao construtor dela e os metodos(conexao com banco)
+// implementa esses OnModules que servem basicamente pra fazer açoes quando o modulo iniciar ou quando ele
+// finalizar(seja por erro inesperado ou afins)
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: ['warn', 'error'],
+      // no super(constructor), ou seja, da classe extendida pai(PrismaClient) temos acesso
+      // ao log do construtor, que podemos passar várias coisas pra ele
+      // neste caso estamos passando warn e error, ou seja, gerará apenas logs de erro e advertências
+      // (pra ficarmos sabendo caso algo de errado)
+    })
+  }
+
+  onModuleInit() {
+    return this.$connect
+    // conecta com o banco quando o módulo iniciar
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect
+    // desconecta do banco quando o módulo finalizar/ser destruido
+  }
+}
