14e07b96f8c437e605f776cf32998d8a46a7b326
Raul <raulantsilveira@gmail.com>
2025-03-12 15:00:36 -0300

    feat: zod validation pipe

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,9 +4,24 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { hash } from 'bcryptjs'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  // criand o schema do objeto usando zod(pra uma posterior validação de campos)
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+// uma das coisas mais sensacionais do zod: ele cria/infere um tipo pra gente
+// entao a gente criou o schema, e com base no schema, usamos o z.infer pra criar
+// um tipo baseado nesse schema
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -14,7 +29,13 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  // esse UsePipes serve pra usarmos uma pipe aqui como quase um middleware no nosso controller
+  // entao passamos o schema pra ele e ele já faz o parse pra ver se esta tudo de acordo antes de
+  // executar o resto do codigo
+  async handle(@Body() body: CreateAccountBodySchema) {
+    // passamos aqui o tipo criado usando zod pra dizer qual formato a gente espera que venha
+    // no body da nossa requisição
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,37 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common'
+import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+// essa biblioteca é pra basicamente deixar os erros do zod mais bonitinhos(legiveis, deixa diferente)
+
+// pipes pelo o que eu entendi são estilo middleware mas pra validação de erro
+// neste caso usamos uma pipe do Zod(que tem la na doc do NestJS) pra verificar
+// se os dados vieram do jeito que a gente quer(de acordo com o schema)
+
+export class ZodValidationPipe implements PipeTransform {
+  // implementa a PipeTransform
+  constructor(private schema: ZodSchema) {}
+  // recebe um schema zod no construtor
+
+  transform(value: unknown) {
+    try {
+      this.schema.parse(value)
+      // tenta usar o parse pra ver se esta tudo ok
+    } catch (error) {
+      // e, se der problema, fazemos a tratativa de erro por aqui mesmo
+      if (error instanceof ZodError) {
+        // se o erro for um erro do Zod, mostramos isso
+        throw new BadRequestException({
+          message: 'Validation failed',
+          // msg personalizda de erro
+          statusCode: 400,
+          // statusCode que queremos mostrar(aparecerá em json na resposta)
+          errors: fromZodError(error),
+          // erro especifico que aconteceu usando o fromZodError pra deixar mais formatadinho
+        })
+      }
+
+      throw new BadRequestException('Validation failed')
+    }
+    return value
+  }
+}
