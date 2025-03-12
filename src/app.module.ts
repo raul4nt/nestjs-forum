0e1d0cd8b6fd0ad4b2debbe1be3d7d60215b02a7
Raul <raulantsilveira@gmail.com>
2025-03-12 15:53:14 -0300

    feat: implement config service to validate env.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+      // torna ele acessivel em todo lugar(todos os modulos, etc)
+    }),
+  ],
+  // forRoot é pra modulos que queremos passar alguma configuração, e o imports é pra importar um modulo
   controllers: [CreateAccountController],
   // são todos os controllers deste módulo
   providers: [PrismaService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { Env } from './env'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3333);
+  const app = await NestFactory.create(AppModule)
+
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  // get nos da acesso a algum serviço da aplicaçao, neste caso o ConfigService
+  // estamos tb dizendo que a tipagem do ConfigService vai ser do tipo Env(que criamos la no env.ts)
+  // que basicamente faz um parse usando o schema do env que nos definimos lá(pra saber oq é necessario de
+  // ter no .env pra aplicaçao rodar sem erros)
+
+  // aquele true do lado de env é o WasValidated, temos que passar pra true por nestjs entender que aquilo é uma
+  // validaçao de campo e que ele pode inferir o tipo sem erro(que port nunca sera undefined)
+
+  const port = configService.get('PORT', { infer: true })
+  // infer = true faz a PORT inferir o tipo que passamos ali na const configService
+
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
