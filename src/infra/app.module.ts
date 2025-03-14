72cffd49159b5bd5e5214fa2ae949267f2a74a9b
Raul <raulantsilveira@gmail.com>
2025-03-14 14:54:53 -0300

    fix: change folders structure

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { PrismaService } from './prisma/prisma.service'
-import { CreateAccountController } from './controllers/create-account.controller'
+
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './controllers/authenticate.controller'
-import { CreateQuestionController } from './controllers/create-question.controller'
-import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+import { HttpModule } from './http/http.module'
 
 @Module({
   imports: [
@@ -16,17 +13,12 @@ import { FetchRecentQuestionsController } from './controllers/fetch-recent-quest
       // torna ele acessivel em todo lugar(todos os modulos, etc)
     }),
     AuthModule,
-    // passando o AuthModule aqui(importando ele tb)
+    HttpModule,
   ],
   // forRoot é pra modulos que queremos passar alguma configuração, e o imports é pra importar um modulo
-  controllers: [
-    CreateAccountController,
-    AuthenticateController,
-    CreateQuestionController,
-    FetchRecentQuestionsController,
-  ],
+
   // são todos os controllers deste módulo
-  providers: [PrismaService],
+
   // todas as dependencia(inversao de dependencia/injectables) que os controllers deste modulo
   // podem vir a ter
 })
