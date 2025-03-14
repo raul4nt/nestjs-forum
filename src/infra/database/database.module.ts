8668f51bed939e2aec1056d6e96791b0aa1e2acc
Raul <raulantsilveira@gmail.com>
2025-03-14 15:16:14 -0300

    feat: create prisma repositories

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+
+@Module({
+  providers: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+  exports: [PrismaService],
+  // exportando pra que eu possa importar o DatabaseModule em outro module e este modulo que esta importando este possa tb usar
+  // o PrismaService. se eu usar apenas providers, ele torna o PrismaService disponivel apenas DIRETAMENTE neste modulo aqui
+  // entao se importar pra outro o PrismaService n funcionaria
+})
+export class DatabaseModule {}
