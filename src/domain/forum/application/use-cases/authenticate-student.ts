082e74444a946961073ace31080694dcd6d16b65
Raul Silveira <raulantsilveira@gmail.com>
2025-03-31 17:00:57 -0300

    finishing authenticate use case

---
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -0,0 +1,53 @@
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { Student } from '../../enterprise/entities/student'
+import { StudentsRepository } from '../repositories/students-repository'
+import { HashGenerator } from '../cryptography/hash-generator'
+import { StudentAlreadyExistsError } from './errors/student-already-exists-error'
+
+interface AuthenticateStudentUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateStudentUseCaseResponse = Either<
+  StudentAlreadyExistsError,
+  {
+    student: Student
+  }
+>
+
+@Injectable()
+export class AuthenticateStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
+    const studentWithSameEmail =
+      await this.studentsRepository.findByEmail(email)
+
+    if (studentWithSameEmail) {
+      return left(new StudentAlreadyExistsError(email))
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password)
+
+    const student = Student.create({
+      name,
+      email,
+      password: hashedPassword,
+    })
+
+    await this.studentsRepository.create(student)
+
+    return right({
+      student,
+    })
+  }
+}
